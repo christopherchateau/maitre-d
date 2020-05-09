91464cce1911d1bba8dd5c76556e7b235409bce9
chris <christopherchateau@gmail.com>
2020-05-09 11:39:00 -0600

    update styling for extra-info in Row

--- src/components/Restaurants/Row.js
+++ src/components/Restaurants/Row.js
@@ -14,12 +14,13 @@ const Row = ({ name, city, state, telephone, genre, attire, website }) => {
 				<h3 className='row-item'>{telephone}</h3>
 				<h3 className='row-item genres'>{genre.join(', ')}</h3>
 			</div>
-			{extraInfoToggle && (
-				<div className='row extra-info'>
-					<h3 className='row-item extra-item'>{attire}</h3>
-					<h3 className='row-item extra-item website'>{website}</h3>
-				</div>
-			)}
+			<div
+				className={'row extra-info'.concat(
+					extraInfoToggle ? '' : ' hide'
+				)}>
+				<h3 className='row-item extra-item'>{attire}</h3>
+				<h3 className='row-item extra-item website'>{website}</h3>
+			</div>
 		</RowTheme>
 	)
 }
@@ -28,7 +29,13 @@ export default Row
 
 const RowTheme = styled.div`
 	background: ${props => props.theme.color.white};
+	border-bottom: 1px solid ${props => props.theme.color.darkgrey};
 	transition: all 0.5s ease-out;
+	word-wrap: break-word;
+
+	&:nth-last-child(2) {
+		border: none;
+	}
 
 	&:hover {
 		cursor: pointer;
@@ -38,13 +45,10 @@ const RowTheme = styled.div`
 
 	.row {
 		align-items: center;
-		border-bottom: 1px solid ${props => props.theme.color.darkgrey};
 		display: flex;
+		height: auto;
 		justify-content: space-between;
-
-		&:nth-last-child(2) {
-			border: none;
-		}
+		overflow: hidden;
 
 		@media (max-width: ${props => props.theme.breakpoint.largephone}) {
 			flex-direction: column;
@@ -69,18 +73,24 @@ const RowTheme = styled.div`
 		}
 	}
 
-    .genres,
-    .website {
+	.genres,
+	.website {
 		@media (max-width: ${props => props.theme.breakpoint.smallphone}) {
 			font-size: ${props => props.theme.font.size.medium};
 		}
 	}
 
+	.extra-info {
+		flex-direction: column;
+		padding-bottom: ${props => props.theme.spacing.large};
+	}
+
 	.extra-item {
-        width: 50%;
-        
-        @media (max-width: ${props => props.theme.breakpoint.largephone}) {
-			width: 100%;
-		}
+		padding: ${props => props.theme.spacing.medium};
+		width: 100%;
+	}
+
+	.hide {
+		display: none;
 	}
 `
