5280dafc1009d694420e4b6c2479743408ae977b
chris <christopherchateau@gmail.com>
2020-05-13 22:15:23 -0600

    Updates

---
+++ precache-manifest.5e78b9635fe0bd808603068c3cc166c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea7e4b37627578a9e82e798e95fee8e",
+    "url": "/maitre-d/index.html"
+  },
+  {
+    "revision": "fa4b07f4280c7712e4f4",
+    "url": "/maitre-d/static/css/main.3700a777.chunk.css"
+  },
+  {
+    "revision": "303a0cac43b7e04797f1",
+    "url": "/maitre-d/static/js/2.51a27c2f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/maitre-d/static/js/2.51a27c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4b07f4280c7712e4f4",
+    "url": "/maitre-d/static/js/main.3e45fd7c.chunk.js"
+  },
+  {
+    "revision": "a9778a7952526927b6fe",
+    "url": "/maitre-d/static/js/runtime-main.81a8bf54.js"
+  },
+  {
+    "revision": "1a8b9aa0902aef8dab24f938e7d0b6bc",
+    "url": "/maitre-d/static/media/background.1a8b9aa0.jpg"
+  },
+  {
+    "revision": "a21746f5374859d98f7528e1857e3532",
+    "url": "/maitre-d/static/media/loading.a21746f5.gif"
+  },
+  {
+    "revision": "f3c07348859246967a5c3c709b74a971",
+    "url": "/maitre-d/static/media/logo-white.f3c07348.png"
+  }
+]);
